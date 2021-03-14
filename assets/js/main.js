ef179f33d4ed8371044812b6dba6e8b1d51d3187
Claudio <claudiocarchietti@yahoo.it>
2021-03-14 22:38:19 +0100

    aggiunte 2 opzioni di  aprire le sotto-finestre
    .Click()  o .mouseEnter() .mouseLeave()

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,104 @@
+//CON MOUSEENTER-LEAVE
+
+// $(document).ready(function() {
+//   var helpbutton = $('.help');
+//   var aboutbutton = $('.about');
+//   var plansbutton = $('.plans');
+//   var featuresbutton = $('.features');
+//
+//   helpbutton.mouseenter(function () {
+//     helpbtn($(this));
+//   })
+//
+//   helpbutton.mouseleave(function () {
+//     $('.list').removeClass('active');
+//   })
+//
+//   aboutbutton.mouseenter(function() {
+//     aboutbtn($(this));
+//   })
+//
+//   aboutbutton.mouseleave(function() {
+//     $('.list').removeClass('active');
+//   })
+//
+//   plansbutton.mouseenter(function() {
+//     plansbtn($(this));
+//   })
+//
+//   plansbutton.mouseleave(function() {
+//     $('.list').removeClass('active')
+//   })
+//
+//   featuresbutton.mouseenter(function() {
+//     featuresbtn($(this));
+//   })
+//
+//   featuresbutton.mouseleave(function() {
+//     $('.list-features').removeClass('active');
+//   })
+//
+// })
+
+//FINE MOUSEENTER-LEAVE
+
+
+
+//CON CLICK
+
 $(document).ready(function() {
+
   var helpbutton = $('.help');
   var aboutbutton = $('.about');
   var plansbutton = $('.plans');
   var featuresbutton = $('.features');
 
-  helpbutton.mouseenter(function () {
-    helpbtn($(this));
+
+  helpbutton.click(function () {
+    if ($('.list').hasClass('active')) {
+      $('.list').removeClass('active');
+    } else {
+      helpbtn($(this));
+    }
   })
 
-  helpbutton.mouseleave(function () {
-    $('.list').removeClass('active');
+  aboutbutton.click(function() {
+    if ($('.list').hasClass('active')) {
+      $('.list').removeClass('active');
+    } else {
+      aboutbtn($(this));
+    }
   })
 
-  aboutbutton.mouseenter(function() {
-    aboutbtn($(this));
+  plansbutton.click(function() {
+    if ($('.list').hasClass('active')) {
+      $('.list').removeClass('active');
+    } else {
+      plansbtn($(this));
+    }
   })
 
-  aboutbutton.mouseleave(function() {
-    $('.list').removeClass('active');
-  })
+  featuresbutton.click(function() {
+    if ($('.list-features').hasClass('active')) {
+      $('.list-features').removeClass('active');
+    } else {
+      featuresbtn($(this));
+    }
 
-  plansbutton.mouseenter(function() {
-    plansbtn($(this));
   })
-
-  plansbutton.mouseleave(function() {
-    $('.list').removeClass('active');
-  })
-
-  featuresbutton.mouseenter(function() {
-    featuresbtn($(this));
-  })
-
-  featuresbutton.mouseleave(function() {
-    $('.list-features').removeClass('active');
-  })
-
 })
+
+//FINE CLICK
+
+
+
+
 
 //FUNCTIONS
 
 function featuresbtn(button){
   button.next().children().css('right','25%');
   button.next().children().addClass('active');
-  console.log($(this));
 }
 
 function plansbtn(button){
